35e05b681b1e1d8e1ef41d7ed6372562b2286a8f
Sanju Kumari <87818231+Sanju2919@users.noreply.github.com>
2025-02-15 23:48:45 +0530

    Update Reverse Integer.py

--- String/Reverse Integer.py
+++ String/Reverse Integer.py
@@ -6,19 +6,21 @@ class Solution:
         z = ''
         n = len(y)     
         # handle negative integers
-        if y[0] == '-': 
-            # for each digit in integer, add it to the front of the answer string
+        if y[0] == '-':         
+            # add it to the front of the answer string
             for i in range(1,n):            
                 z = y[i] + z
             z = '-' + z    
         # handle positive integers
-        else:
-            # for each digit in integer, add it to the front of the answer string
+        else:            
+            # add it to the front of the answer string
             for i in range(0,n):            
                 z = y[i] + z
             
         revnum = int(z)
-        # TODO: handle edge cases: when reversed integer <-2 raised 31 or >2 raised 31
-
-        return revnum
+        # handle edge cases: when reversed integer <-2 raised 31 or >2 raised 31
+        if revnum > (( 2 ** 31 ) - 1 ) or revnum < -(2 ** 31):
+            return 0
+        else:
+            return revnum
         
